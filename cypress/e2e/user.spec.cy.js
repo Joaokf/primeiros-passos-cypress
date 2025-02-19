8fa198de618881a4b44168de20cf2826df3e647c
João Augusto Arruda <joaokf01@gmail.com>
2025-02-19 16:49:30 -0300

    add solução aleatória para todos os campos

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -1,4 +1,3 @@
-
 import userData from '../fixtures/user-data.json'
 import LoginPage from '../pages/loginPage'
 import DashboardPage from '../pages/dashboardPage'
@@ -6,21 +5,14 @@ import MenuPage from '../pages/menuPage'
 import MyInfoPage from '../pages/myInfoPage'
 
 const Chance = require('chance');
+const chance = new Chance();
 
-var chance = new Chance();
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
 const myInfoPage = new MyInfoPage()
 
-
 describe('Orange HRM Tests', () => {
-
-  // const selectorsList = {
-
-  // }
-  
-
   it('User Info Update - Success', () => {
     loginPage.accessLoginPage()
     loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
@@ -30,16 +22,33 @@ describe('Orange HRM Tests', () => {
     menuPage.accessMyInfo()
     // 'Middle Name'
     myInfoPage.fillPersonalDetails(chance.first(), chance.string(), chance.last())
-    myInfoPage.fillEmployeeDetails(chance.profession(), 'Other Id', 'Driver License Number', '2025-07-29')
+    
+    // Gerando uma data formatada no formato YYYY-MM-DD
+    const formattedDate = chance.date({ year: 2025 }).toLocaleDateString('en-CA') // Formato canadense (YYYY-MM-DD)
+
+    myInfoPage.fillEmployeeDetails(chance.integer({ min: 1000000000, max: 9999999999 }), chance.integer({ min: 1000000000, max: 9999999999 }), chance.integer({ min: 1000000000, max: 9999999999 }), formattedDate)
     myInfoPage.fillStatus()    
     myInfoPage.saveForm()
-    myInfoPage.personalStatus('Brazilian', 'Single', '1992-08-27', 'Male')
-    myInfoPage.customField('O-', 'Test Value')
+
+    // Usando chance.bool() para selecionar aleatoriamente entre 'Single' ou 'Married'
+    const maritalStatus = chance.bool() ? 'Single' : 'Married'
+    const dateOfBirth = chance.birthday({ age: 25 }).toLocaleDateString('en-CA')
+    cy.wait(500);
+
+    // ---------------------------------------------------------------------------------------------------------------
+    const nationality = chance.country({ full: true });  // Nacionalidade completa (nome do país)
+    console.log("Selected nationality: ", nationality); // Verifique o valor da nacionalidade gerada
+
+    myInfoPage.personalStatus(nationality, maritalStatus, dateOfBirth, chance.gender());
+    cy.wait(500)
+    // ---------------------------------------------------------------------------------------------------------------
+
+    // TIPO SANGUÍNEO
+    const bloodType = chance.pickone(['A+', 'A-', 'B+', 'B-', 'AB+', 'AB-', 'O+', 'O-']);
+    console.log("Selected blood type: ", bloodType); // Verifique o tipo sanguíneo gerado
+
+    myInfoPage.customField(bloodType, 'Test Value')
     myInfoPage.saveFormCustomField()
     myInfoPage.checkboxField()
-   
   })
-
 })
-
-
